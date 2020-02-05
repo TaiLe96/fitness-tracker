2835895efead20e3c1f824faf4f8a617bbd575a7
Tai Le <tle449@gmail.com>
2020-02-05 15:07:34 -0800

    initial commit

--- server.js
+++ server.js
@@ -9,13 +9,22 @@ const PORT = process.env.PORT || 1000
 const app = express();
 
 app.use(logger("dev"))
+
+require('dotenv').config();
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/htmlRoutes")(app, path)
 require("./routes/apiRoutes")(app);
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParse: true});
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParse: true});
 
 app.listen(PORT, () =>{
     console.log("Listening on PORT " + PORT)
