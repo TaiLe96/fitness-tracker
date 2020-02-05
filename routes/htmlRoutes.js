d491060eb9b4fe2c6cddff7e419adbcbeb6973d6
Tai Le <tle449@gmail.com>
2020-02-04 18:03:16 -0800

    added server.js to test

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = (app, path) => {
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirnam, "../public/index.html"))
+        res.sendFile(path.join(__dirname, "../public/index.html"))
     });
     
     app.get("/exercise", (req, res) => {
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path")
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.unsubscribe(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/htmlRoutes")(app, path)
+mongoose.connect(process.env.mongoose_uri || "mongodb://localhost/workout", {useNewUrlParse: true});
+
+app.listen(PORT, () =>{
+    console.log("Listening on PORT " + PORT)
+})
