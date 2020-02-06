c3ee7b2a85739ed5e3ff7588155f9548f07771a7
Tai Le <tle449@gmail.com>
2020-02-06 11:48:12 -0800

    initial commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/htmlRoutes")(app, path)
-require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app, path);
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, { useNewUrlParser: true });
 const connection = mongoose.connection;
