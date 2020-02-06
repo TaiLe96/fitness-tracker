fa39d7a4887e504b9d8254d5d94f62da37fba552
Tai Le <tle449@gmail.com>
2020-02-06 12:15:22 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path")
-
+const cors = require("cors")
 const logger = require("morgan")
 
 const PORT = process.env.PORT || 1000
@@ -12,19 +12,20 @@ app.use(logger("dev"))
 
 require('dotenv').config();
 
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/htmlRoutes")(app)
+require("./routes/htmlRoutes")(app, path)
 require("./routes/apiRoutes")(app);
-const uri = process.env.MONGO_URI;
-mongoose.connect(uri, { useNewUrlParser: true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParse: true});
+// const uri = process.env.MONGO_URI;
+// mongoose.connect(uri, { useNewUrlParser: true });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log('MongoDB database connection established successfully');
+// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParse: true});
 
 app.listen(PORT, () =>{
     console.log("Listening on PORT " + PORT)
